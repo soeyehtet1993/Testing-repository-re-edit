e1de0a71e6ee996967fb22bc213c64865b09e76d
IoTcloudServe <iotcloudserve@chula.ac.th>
2019-09-23 14:35:21 +0700

    Testing_from_Mac_Different_Account

--- testing.py
+++ testing.py
@@ -1,7 +1,4 @@
 print("This is testing purpose only will be deleted soon")
-<<<<<<< HEAD
 print("This is new added print line for testing purpose")
 print("This is added by different account")
-=======
-print("This is new added file")
->>>>>>> 8c0bbaf35fb5e669d736aed88a400242427c46c5
+print("This is new added file_from iotcloudserve")
